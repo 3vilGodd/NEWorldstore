0b4110f3b884146b5b186c26871aaf4b99d8d64d
elontusker <me21b2024@iiitdm.ac.in>
2026-01-28 00:19:54 +0530

    Remove: Delete unused prisma/config.ts

--- prisma/config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "prisma";
-
-export default defineConfig({
-  datasource: {
-    url: process.env.DATABASE_URL || "postgresql://user:password@localhost:5432/neworld_estore?schema=public",
-  },
-});
